5e3688b06ed58d2eb928f9937fae36d5c3d92470
Victor Hugo Saavedra <vhspiceros@gmail.com>
2023-05-13 11:13:53 -0400

    add fzf

--- lua/vhspiceros/plugins.lua
+++ lua/vhspiceros/plugins.lua
@@ -16,4 +16,14 @@ Plug 'ryanoasis/vim-devicons'
 
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
+Plug 'neoclide/coc.nvim'
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+
 vim.call('plug#end')
+
