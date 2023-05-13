044b01227dd3ee810ef443e1a8b9f1ba7fc60c7d
Victor Hugo Saavedra <vhspiceros@gmail.com>
2023-05-13 12:20:30 -0400

    Add some configurations

--- lua/vhspiceros/plugins.lua
+++ lua/vhspiceros/plugins.lua
@@ -7,16 +7,14 @@ Plug 'tpope/vim-surround'
 Plug 'tp:pe/vim-repeat'
 Plug 'preservim/nerdtree'
 Plug 'mhinz/vim-startify'
-
 Plug 'vim-airline/vim-airline'
 Plug 'https://github.com/vim-airline/vim-airline-themes'
 Plug 'ryanoasis/vim-devicons'
 
-
-
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
-Plug 'neoclide/coc.nvim'
+Plug 'nvim-telescope/telescope-file-browser.nvim'
+Plug 'junegunn/fzf.vim'
 
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/cmp-nvim-lsp'
@@ -24,6 +22,13 @@ Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
+
+Plug 'onsails/lspkind-nvim' -- vscode-like pictograms
+Plug 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+Plug 'williamboman/nvim-lsp-installer'
+Plug 'neovim/nvim-lspconfig' -- LSP
+Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'glepnir/lspsaga.nvim' -- LSP UIs
 
 vim.call('plug#end')
 
